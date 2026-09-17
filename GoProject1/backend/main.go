3a46bb5286e42dd2b51c7d07f1dcef0312ce5777
lyh27 <lyh27@local>
2026-09-17 11:59:30 +0800

    完善程序关闭

--- GoProject1/backend/main.go
+++ GoProject1/backend/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"exchangeapp/config"
 	"exchangeapp/router"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -13,6 +19,27 @@ func main() {
 	if port == "" {
 		port = ":8080"
 	}
-	r.Run(port)
-	
+	// r.Run(port)
+	srv := &http.Server{
+		Addr:    port,
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
